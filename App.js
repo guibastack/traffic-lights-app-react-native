888f33109ba095700c94503f98d1690bc18295c0
Guilherme Barros <135251907+guibastack@users.noreply.github.com>
2023-06-06 13:51:10 -0300

    Creation of the application's route structure.

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import EmailScreen from './src/Screens/EmailScreen';
+import AuthTokenScreen from './src/Screens/AuthTokenScreen';
+import ChangeNameScreen from './src/Screens/ChangeNameScreen';
+import NearbyTrafficLightsScreen from './src/Screens/NearbyTrafficLightsScreen';
+import MapTrafficLightsScreen from './src/Screens/MapTrafficLightsScreen';
+import SettingsScreen from './src/Screens/SettingsScreen';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+export default () => {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    <NavigationContainer>
+
+      <Stack.Navigator initialRouteName="SettingsScreen">
+        <Stack.Screen name="EmailScreen" component={EmailScreen}></Stack.Screen>
+        <Stack.Screen name="AuthTokenScreen" component={AuthTokenScreen}></Stack.Screen>
+        <Stack.Screen name="ChangeNameScreen" component={ChangeNameScreen}></Stack.Screen>
+        <Stack.Screen name="NearbyTrafficLightsScreen" component={NearbyTrafficLightsScreen}></Stack.Screen>
+        <Stack.Screen name="MapTrafficLightsScreen" component={MapTrafficLightsScreen}></Stack.Screen>
+        <Stack.Screen name="SettingsScreen" component={SettingsScreen}></Stack.Screen>
+      </Stack.Navigator>
+
+    </NavigationContainer>
+
+  );
+
+}
