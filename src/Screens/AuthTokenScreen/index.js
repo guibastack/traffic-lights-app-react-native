a1c1e2fce20d5b82b5ef01723812face7e359cf9
Guilherme Barros <135251907+guibastack@users.noreply.github.com>
2023-06-08 14:04:19 -0300

    Token screen design.

--- src/Screens/AuthTokenScreen/index.js
+++ src/Screens/AuthTokenScreen/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import * as Styles from '../../Styles/Styles';
+import ButtonLabel from '../../Components/ButtonLabel';
+import InputLabel from '../../Components/InputLabel';
 
 export default () => {
 
     return (
 
         <View style={[Styles.styles.appContainer]}>
-            <Text>Auth Token Screen</Text>
+
+            <View style={[Styles.styles.MB8]}>
+                <InputLabel 
+                    label="Enter your token"
+                    placeholder="abcd123"
+                    inputMode="text"
+                />
+            </View>
+
+            <View>
+                <ButtonLabel 
+                    label="Ok"
+                />
+            </View>
+
         </View>
 
     );
