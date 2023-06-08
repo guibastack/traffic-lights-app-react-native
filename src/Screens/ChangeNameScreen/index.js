8bd634d7faafe4dbe32688a051ef8774b26c02a9
Guilherme Barros <135251907+guibastack@users.noreply.github.com>
2023-06-08 14:37:16 -0300

    ChangeName screen design.

--- src/Screens/ChangeNameScreen/index.js
+++ src/Screens/ChangeNameScreen/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import * as Styles from '../../Styles/Styles';
+import InputLabel from '../../Components/InputLabel';
+import ButtonLabel from '../../Components/ButtonLabel';
 
 export default () => {
 
     return (
 
         <View style={[Styles.styles.appContainer]}>
-            <Text>Name Screen.</Text>
+
+            <View style={[Styles.styles.MB8]}>
+                <InputLabel 
+                    label="Enter your name"
+                    placeholder="Marie"
+                    inputMode="text"
+                />
+            </View>
+
+            <View>
+                <ButtonLabel
+                    label="Change"
+                />
+            </View>
+
         </View>
 
     );
